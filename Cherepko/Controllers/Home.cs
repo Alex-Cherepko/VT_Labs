30c680f2e15416ad73023c87049f66372965182c
Alexander Cherepko <alexxcherepko@gmail.com>
2021-05-31 23:23:24 +0300

    Лаб 2 п 5.5

--- Cherepko/Controllers/Home.cs
+++ Cherepko/Controllers/Home.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,25 @@ namespace Cherepko.Controllers
 {
     public class Home : Controller
     {
+        private List<DemoList> listDemo;
         public IActionResult Index()
         {
+            FillDemoList();
+
+            ViewData["Text"] = "Лабораторная работа 2";
+            ViewData["Lst"] = new SelectList(listDemo, "ListItemValue", "ListItemText");
+
             return View();
+        }
+
+            public void FillDemoList()
+            {
+                listDemo = new List<DemoList>
+                {
+                new DemoList{ ListItemValue=1, ListItemText="Item 1"},
+                new DemoList{ ListItemValue=2, ListItemText="Item 2"},
+                new DemoList{ ListItemValue=3, ListItemText="Item 3"}
+                };
         }
     }
 }
---
+++ Cherepko/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cherepko.Controllers
+{
+    public class DemoList
+    {
+        public int ListItemValue { get; set; }
+        public string ListItemText { get; set; }
+    }
+}
